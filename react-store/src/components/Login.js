4b11135732212ebfb8c769c32b1a4b1cd37070b4
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-22 19:42:33 -0400

    first react component

---
+++ react-store/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//login component
+class Login extends React.Component {
+
+    render(){
+        return <form classname='login-wrapper'> </form>; //JSX Babel emmet
+    }
+}
+
+//export Login component
+export default Login;

c6ac591966bfc0aa4017e52f485305c518d8412e
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-22 20:05:28 -0400

    write html using JSX

--- react-store/src/components/Login.js
+++ react-store/src/components/Login.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
 
 //login component
 class Login extends React.Component {
+  render() {
+    return (
+      <form className="login-wrapper">
+        <div>
+          {/*email label*/}
+          <label>Email</label> 
+          {/*email input field*/}
+          <div>
+            <input type="text" placeholder="Email" />
+          </div>
+        </div>
 
-    render(){
-        return <form classname='login-wrapper'> </form>; //JSX Babel emmet
-    }
+        <div>
+          {/*password label*/}
+          <label>Password</label> 
+          {/*password input field*/}
+          <div>
+            <input type="text" placeholder="Password" />
+          </div>
+        </div>
+        {/*Sumbit button*/}
+        <div>
+            <button>Submit</button>
+        </div>
+      </form>
+    ); //JSX Babel emmet
+  }
 }
 
 //export Login component

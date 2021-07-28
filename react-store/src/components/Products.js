12d86bfb2fbee7abe233dd213d5df7c7f078e9b4
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-27 21:46:14 -0400

    searching function

--- react-store/src/components/Products.js
+++ react-store/src/components/Products.js
@@ -5,21 +5,42 @@ import Product from 'components/Product';
 
 class Products extends React.Component {
   state = {
-    products: []
+    products: [],
+    sourceProducts: []
   };
 
   componentDidMount() {
     axios.get('/products').then(response => {
       this.setState({
-        products: response.data
+        products: response.data,
+        sourceProducts: response.data
       });
     });
   }
 
+  // search
+  search = text => {
+    // 1. retrieved new array
+    let _products = [...this.state.sourceProducts];
+
+    // 2.filter the retrieved array
+    _products = _products.filter(p => {
+      // name: Abcd text: ab   ===> ['Ab']
+      // text: '' ==> ["", "", "", "", ""]
+      const matchArray = p.name.match(new RegExp(text, 'gi'));
+      return !!matchArray;
+    });
+
+    // 3. set State
+    this.setState({
+      products: _products
+    });
+  };
+
   render() {
     return (
       <div>
-        <ToolBar />
+        <ToolBar search={this.search} />
         <div className="products">
           <div className="columns is-multiline is-desktop">
             {this.state.products.map(p => {
--- react-store/src/components/ToolBar.js
+++ react-store/src/components/ToolBar.js
@@ -2,7 +2,28 @@ import React from "react";
 
 class ToolBar extends React.Component {
 
-  state 
+  state = {
+    //declare a controlled input
+    searchText: ''
+  };
+
+  handleChange = e => {
+    //update seatchText input state
+    const value = e.target.value;
+    this.setState({
+      searchText: value
+    });
+    this.props.search(value);
+  };
+
+  clearSearchText = () => {
+    //set seatchText input state to empty 
+    this.setState({
+      searchText: ''
+    });
+    this.props.search('');
+  };
+
   render() {
     return (
       <div className="tool-box">
@@ -15,15 +36,18 @@ class ToolBar extends React.Component {
               <input
                 type="text"
                 className="input search-input"
-                placeholder="Search Product..."
+                placeholder="Search Product"
+                value={this.state.searchText}
+                onChange={this.handleChange}
               />
             </div>
             <div className="control">
-              <button className="button">X</button>
+              <button className="button" onClick={this.clearSearchText}>
+                X
+              </button>
             </div>
           </div>
         </div>
-        {/*Shopping Cart*/}
         <div className="cart-box">
           <i className="fas fa-shopping-cart"></i>
           <span className="cart-num">(0)</span>

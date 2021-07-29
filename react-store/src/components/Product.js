bd94d145b0bfb57576d013369578f7177ce40ff3
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-28 22:10:52 -0400

    add to cart 1

--- react-store/src/components/Product.js
+++ react-store/src/components/Product.js
@@ -1,33 +1,63 @@
 import React from "react";
-import Panel from 'components/Panel';
+import axios from 'commons/axios';
+import { toast } from 'react-toastify';
+import Panel from "components/Panel";
 import { formatPrice } from "commons/helper";
-import EditInventory from 'components/EditInventory';
+import EditInventory from "components/EditInventory";
 
 class Product extends React.Component {
-
-  //edit inventory method 
+  //edit inventory method
   toEdit = () => {
     Panel.open({
       component: EditInventory,
       props: {
         product: this.props.product,
-        deleteProduct: this.props.delete
+        deleteProduct: this.props.delete,
       },
-      callback: data => {
+      callback: (data) => {
         if (data) {
+          //update new data after edit/delete
           this.props.update(data);
         }
-      }
+      },
     });
   };
 
+  addCart = async () => {
+    try {
+      //Declare product fields
+      const { id, name, image, price } = this.props.product;
+      const res = await axios.get(`/carts?productId=${id}`);
+      const carts = res.data;
+      if (carts && carts.length > 0) {
+        const cart = carts[0];
+        cart.mount += 1;
+        await axios.put(`/carts/${cart.id}`, cart);
+      } else {
+        const cart = {
+          productId: id,
+          name,
+          image,
+          price,
+          mount: 1 //add 1 item to cart when clicked
+        };
+        await axios.post('/carts', cart);
+      }
+      toast.success('Add Cart Success');
+    } catch (error) {
+      toast.error('Add Cart Failed');
+    }
+  };
 
   render() {
+    //Declare product fields
     const { name, image, tags, price, status } = this.props.product;
+    //Decalre product states
     const _pClass = {
-      available: 'product',
-      unavailable: 'product out-stock'
+      available: "product",
+      unavailable: "product out-stock",
     };
+
     return (
       <div className={_pClass[status]}>
         <div className="p-content">
@@ -49,9 +79,16 @@ class Product extends React.Component {
           <p className="p-tags">{tags}</p>
           <p className="p-name">{name}</p>
         </div>
+        {/*Product footer*/}
         <div className="p-footer">
+          {/*Price*/}
           <p className="price">{formatPrice(price)}</p>
-          <button className="add-cart" disabled={status === "unavailable"}>
+          {/*Add to cart button*/}
+          <button
+            className="add-cart"
+            disabled={status === "unavailable"}
+            onClick={this.addCart}
+          >
             <i className="fas fa-shopping-cart"></i>
             <i className="fas fa-exclamation"></i>
           </button>

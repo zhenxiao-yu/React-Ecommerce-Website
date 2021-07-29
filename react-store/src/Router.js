97106d336519e7e96f4ff787553a6ff87fe65714
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-29 15:03:31 -0400

    cart item list template

--- react-store/src/Router.js
+++ react-store/src/Router.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 //router default
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 //main page
-import App from 'pages/App';
+import App from "pages/App";
 //login page
-import Login from 'pages/Login';
+import Login from "pages/Login";
 //not found page
-import NotFound from 'pages/NotFound';
+import NotFound from "pages/NotFound";
 
-import Cart from 'pages/Cart';
+import Cart from "pages/Cart";
 
 const Router = () => (
   <BrowserRouter>
@@ -17,9 +17,9 @@ const Router = () => (
       <Route path="/" exact component={App} />
       {/*path to login page*/}
       <Route path="/login" component={Login} />
-      {/*direct undefined pages to not found page*/}
-
+      {/*path to cart page*/}
       <Route path="/cart" component={Cart} />
+      {/*direct undefined pages to not found page*/}
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>

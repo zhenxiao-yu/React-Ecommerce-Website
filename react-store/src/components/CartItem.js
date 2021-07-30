49d643c608db6c7e0eff71ab222e359eb23dda48
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-29 22:42:27 -0400

    component reroute

--- react-store/src/components/CartItem.js
+++ react-store/src/components/CartItem.js
@@ -1,14 +1,36 @@
-import React from "react";
-import { formatPrice } from "commons/helper";
+import React, { useState, useMemo } from 'react';
+import axios from 'commons/axios';
+import { formatPrice } from 'commons/helper';
 
-const CartItem = (props) => {
-  //Define cart item fields
-  const { name, image, price, mount } = props.cart || {};
-  //Calculate total price of an item
-  const sumPrice = formatPrice(mount * parseInt(price));
+const CartItem = props => {
+  const [mount, setMount] = useState(props.cart.mount);
+  const { id, name, image, price } = props.cart || {};
+
+  const sumPrice = useMemo(() => {
+    return formatPrice(mount * parseInt(price));
+  }, [mount, price]);
+
+  const handleChange = e => {
+    const _mount = parseInt(e.target.value);
+    setMount(_mount);
+    const newCart = {
+      ...props.cart,
+      mount: _mount
+    };
+    axios.put(`/carts/${id}`, newCart).then(res => {
+      props.updateCart(newCart);
+    });
+  };
+
+  const deleteCart = () => {
+    axios.delete(`/carts/${id}`).then(res => {
+      props.deleteCart(props.cart);
+    });
+  };
+
   return (
     <div className="columns is-vcentered">
-      <div className="column is-narrow">
+      <div className="column is-narrow" onClick={deleteCart}>
         <span className="close">X</span>
       </div>
       <div className="column is-narrow">
@@ -19,7 +41,13 @@ const CartItem = (props) => {
         <span className="price">{formatPrice(price)}</span>
       </div>
       <div className="column">
-        <input type="number" className="input num-input" defaultValue={mount} />
+        <input
+          type="number"
+          className="input num-input"
+          min={1}
+          value={mount}
+          onChange={handleChange}
+        />
       </div>
       <div className="column">
         <span className="sum-price">{sumPrice}</span>

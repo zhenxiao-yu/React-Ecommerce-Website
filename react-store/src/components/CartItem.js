62938a3025aa01f6dc133daced03062b1620332d
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-29 20:08:51 -0400

    Revert "shopping cart function complete"
    This reverts commit 3a1edfe6237273f5fc3cef59fbac7897811f8533.

--- react-store/src/components/CartItem.js
+++ react-store/src/components/CartItem.js
@@ -1,37 +1,14 @@
-import React, { useState, useMemo } from 'react';
-import axios from 'commons/axios';
-import { formatPrice } from 'commons/helper';
+import React from "react";
+import { formatPrice } from "commons/helper";
 
-const CartItem = props => {
-  const [mount, setMount] = useState(props.cart.mount);
-  const { id, name, image, price } = props.cart || {};
-
-  const sumPrice = useMemo(() => {
-    return formatPrice(mount * parseInt(price));
-  }, [mount, price]);
-
-  const handleChange = e => {
-    const _mount = parseInt(e.target.value);
-    setMount(_mount);
-    //change API values
-    const newCart = {
-      ...props.cart,
-      mount: _mount
-    };
-    axios.put(`/carts/${id}`, newCart).then(res => {
-      props.updateCart(newCart);
-    });
-  };
-
-  const deleteCart = () => {
-    axios.delete(`/carts/${id}`).then(res => {
-      props.deleteCart(props.cart);
-    });
-  };
-
+const CartItem = (props) => {
+  //Define cart item fields
+  const { name, image, price, mount } = props.cart || {};
+  //Calculate total price of an item
+  const sumPrice = formatPrice(mount * parseInt(price));
   return (
     <div className="columns is-vcentered">
-      <div className="column is-narrow" onClick={deleteCart}>
+      <div className="column is-narrow">
         <span className="close">X</span>
       </div>
       <div className="column is-narrow">
@@ -42,13 +19,7 @@ const CartItem = props => {
         <span className="price">{formatPrice(price)}</span>
       </div>
       <div className="column">
-        <input
-          type="number"
-          className="input num-input"
-          min={1}
-          value={mount}
-          onChange={handleChange}
-        />
+        <input type="number" className="input num-input" defaultValue={mount} />
       </div>
       <div className="column">
         <span className="sum-price">{sumPrice}</span>
